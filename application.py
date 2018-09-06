83997eae5a57aa9f3a1af693154ed96bc83bc164
Chris Hannam <chris.hannam@babylonhealth.com>
2018-09-06 09:43:24 +0100

    latest file templates

--- application.py
+++ application.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask import render_template
 
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
+
+def say_hello(username="World"):
+    return render_template('index.html', name=username)
+
+
+def login():
+    return render_template('login.html')
+
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
+
+
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+application.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
 
 # some bits of text for the page.
 header_text = '''
@@ -14,17 +28,12 @@ instructions = '''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
 
 # add a rule for the index page.
 application.add_url_rule('/', 'index', (lambda: header_text +
     say_hello() + instructions + footer_text))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+application.add_url_rule('/<username>', view_func=login)
 
 # run the app.
 if __name__ == "__main__":
